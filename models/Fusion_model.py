f1e929da640a3ba0cddf6167c70d780f185de8e3
julian <zjulian926@gmail.com>
2021-03-04 11:03:28 +0800

    align

--- models/Fusion_model.py
+++ models/Fusion_model.py
@@ -195,12 +195,13 @@ class FusionModel(DistangleModel):
         base_shadow_output = shadow_image * base_shadow_param_pred[:, :3].view((n, 3, 1, 1)) + \
                              base_shadow_param_pred[:, 3:].view((n, 3, 1, 1))
         shadow_output_list = []
+        step_length = 0.025
         for i in range(0, self.n - 1):
             if i % 2 == 0:
-                scale = 1 + i * 0.01
+                scale = (1 + i // 2) * step_length
             else:
-                scale = 1 - i * 0.01
-            shadow_output_list.append(base_shadow_output * scale)
+                scale = -(1 + i // 2) * step_length
+            shadow_output_list.append(base_shadow_output * (1 + scale))
         shadow_output = torch.cat([base_shadow_output] + shadow_output_list, axis=1)
         self.lit = torch.cat([base_shadow_output] + shadow_output_list, axis=-1) * 2 - 1
 
